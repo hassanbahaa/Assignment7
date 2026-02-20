2971d1d9c793aba3b46612214ad47ff00b0ff224
hassanbahaa <hassanbahaa45@gmail.com>
2026-02-20 19:50:20 +0200

    feat:update comment

--- src/modules/comment/comment.controller.js
+++ src/modules/comment/comment.controller.js
@@ -1,11 +1,10 @@
 const router = require("express").Router();
 const { Comment } = require("../../db/models");
-const {createComment,createComments } = require("./comment.service");
+const {createComment,createComments, updateComment } = require("./comment.service");
 
 
 router.post("/", async (req, res) => {
     const comments = req.body.comments;
-    
 try {
         const newComments = await createComments(comments);
     res.status(201).json({
@@ -16,8 +15,24 @@ try {
     res
       .status(error.cause || 500)
       .json({ message: error.message, success: false });
-}
-   
+}});
 
-})
+
+router.patch("/:id",async (req,res)=>{
+    const commentId = req.params.id;
+
+    const  {postId,...data}  = req.body;
+    try {
+        const result = await updateComment(commentId,data);
+        console.log(result);
+        res.status(200).json({
+            message: "Comment updated successfully",
+            success: true,
+          });
+    } catch (error) {
+        res
+          .status(error.cause || 500)
+          .json({ message: error.message, success: false });
+    }
+});
 module.exports = { commentRouter: router };
