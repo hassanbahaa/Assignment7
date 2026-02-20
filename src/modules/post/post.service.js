c90825b71eac87935b5af64c715b07abffa8c22f
hassanbahaa <hassanbahaa45@gmail.com>
2026-02-20 17:00:17 +0200

    feat:delete post with id

--- src/modules/post/post.service.js
+++ src/modules/post/post.service.js
@@ -11,6 +11,30 @@ async function createPost(post) {
   return newPost;
 }
 
+async function deletePost(postId,userId) {
+  if (!userId) throw new Error("User Id is required", { cause: 400 });
+  // check if post exist
+  const postExist = await checkPostExist({ id: postId });
+  if (!postExist) throw new Error("Post not found", { cause: 404 });
+  // check for authorship
+
+  const post = await Post.findOne({ where: { id: postId } });
+  console.log({postId: postId, userId: userId, post: post.userId});
+  if (post.userId != userId) throw new Error("you are not authorized to delete this post", { cause: 403 });
+
+  await Post.destroy({ where: { id: postId } });
+  return true;
+  
+}
+
+async function checkPostExist(filter) {
+  const result = await Post.findOne({ where: filter });
+  if (!result) return false;
+  return true;
+}
+
+
 module.exports = {
   createPost,
+  deletePost
 };
